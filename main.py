bc1d228e1272aac3376be4f0e685a86b46a4556a
zhuang002 <zhuang001@gmail.com>
2022-12-17 20:10:54 -0500

    final version

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ def readinMap() -> list[list[str]]:
                 start = (i, j)
             if row[j] == 'C':
                 cameras.append((i, j))
-    return start, myMap
+    return start, cameras, myMap
 
 
 def mark(camera: tuple[int, int], dRow: int, dCol: int, myMap: list[list[str]], myStep: list[list[int]]) -> None:
     r, c = camera
-    while 0 <= r <= len(myMap) and 0 <= c <= len(myMap[0]) and myStep[r][c] != 'W':
+    while 0 <= r < len(myMap) and 0 <= c < len(myMap[0]) and myMap[r][c] != 'W':
         if myMap[r][c] == '.' or myMap[r][c] == 'S':
-            stepMap[r][c] = -2
+            myStep[r][c] = -2
         r += dRow
         c += dCol
 
@@ -48,24 +48,28 @@ def move(loc: tuple[int, int], dRow: int, dCol: int, myMap: list[list[str]],
         return None
     s = myMap[r][c]
     if s == '.':
-        myMap[r][c] = step
-        return (r, s)
+        stepMap[r][c] = step
+        return (r, c)
     if s == 'W':
         return None
 
     if s == 'U':
-        move((r, c), -1, 0, myMap, stepMap, step)
+        stepMap[r][c] = -2
+        return move((r, c), -1, 0, myMap, stepMap, step)
     elif s == 'D':
-        move((r, c), 1, 0, myMap, stepMap, step)
+        stepMap[r][c] = -2
+        return move((r, c), 1, 0, myMap, stepMap, step)
     elif s == 'L':
-        move((r, c), 0, -1, myMap, stepMap, step)
+        stepMap[r][c] = -2
+        return move((r, c), 0, -1, myMap, stepMap, step)
     elif s == 'R':
-        move((r, c), 0, 1, myMap, stepMap, step)
+        stepMap[r][c] = -2
+        return move((r, c), 0, 1, myMap, stepMap, step)
 
 
 def bfs(start: tuple[int, int], myMap: list[list[str]], stepMap: [list[list[int]]]) -> None:
     if stepMap[start[0]][start[1]] == -1:
-        stepMap[start[0]][start[1]] == 0
+        stepMap[start[0]][start[1]] = 0
     else:
         return
 
@@ -94,7 +98,10 @@ def bfs(start: tuple[int, int], myMap: list[list[str]], stepMap: [list[list[int]
 start, cameras, myMap = readinMap()
 stepMap = preProcessCameras(myMap, cameras)
 bfs(start, myMap, stepMap)
-for i in len(myMap):
-    for j in len(myMap[0]):
+for i in range(len(myMap)):
+    for j in range(len(myMap[0])):
         if myMap[i][j] == '.':
-            print(stepMap[i][j])
+            if stepMap[i][j]>=0:
+                print(stepMap[i][j])
+            else:
+                print(-1)
